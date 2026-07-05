9a0c6c82352c77544faedbed4d51721c260e8e89
dumbrepranav1010-spec <dumbrepranav1010@gmail.com>
2026-07-05 13:53:52 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('Hello world!')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # host='0.0.0.0' allows connections from outside the container
+    app.run(host='0.0.0.0', port=5000)
